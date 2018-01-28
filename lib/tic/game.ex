d0a42a60f4211d4ac338b77f46b6abef9bc656f2
Joseph An <an.joseph891@gmail.com>
2018-01-28 13:57:37 -0500

    add game joining

--- lib/tic/game.ex
+++ lib/tic/game.ex
@@ -16,6 +16,10 @@ defmodule Tic.Game do
     GenServer.call(pid, :get_id)
   end
 
+  def join(game_id) do
+    GenServer.call(process(game_id), :join)
+  end
+
   # Server
 
   def init(game_id) do
@@ -29,5 +33,18 @@ defmodule Tic.Game do
 
   def handle_call(:get_id, _from, state) do
     {:reply, Map.get(state, :id), state}
+  end
+
+  def handle_call(:join, _from, state) do
+    cond do
+      Map.get(state, :player1) == nil ->
+        id = UUID.uuid1()
+        {:reply, {:ok, id}, Map.put(state, :player1, id)}
+      Map.get(state, :player2) == nil ->
+        id = UUID.uuid1()
+        {:reply, {:ok, id}, Map.put(state, :player2, id)}
+      true ->
+      {:reply, {:error, "Game is full"}, state}
+    end
   end
 end
